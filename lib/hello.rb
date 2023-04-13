fd0eaa376f91001697e3a2ed750a806d093f7f65
Evgeniia Pankova <jen.backulina@yandex.ru>
2023-04-13 13:24:58 +0200

    Hello uses Greeter

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,5 +1,7 @@
-# Default is "World"
-# Author: Evgeniia Pankova (jen.backulina@yandex.ru)
+require 'greeter'
+
+# Default is World
 name = ARGV.first || "World"
 
-puts "Hello, #{name}!"
+greeter = Greeter.new(name)
+puts greeter.greet
